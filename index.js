21bcb3323f1549d5358ea50990da23babaceed78
Moataz Maher <moatazmaher1998@gmail.com>
2021-04-24 17:25:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ var s3 = new AWS.S3({
     secretAccessKey: SECRET_ACCESS_KEY,
 })
 var params = {
-    Key: 'w.h5',
+    Key: 'weights.h5',
     Bucket: BUCKET_NAME
 }
 s3.getObject(params, function(err, data) {
     if (err) {
         throw err
     }
-    fs.writeFileSync('./w.h5', data.Body)
+    fs.writeFileSync('./weights.h5', data.Body)
     console.log('file downloaded successfully')
 
 })
