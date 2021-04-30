cfe23477cd8bcf7ec3adfe91a1dd3e87d335ea4e
Moataz Maher <moatazmaher1998@gmail.com>
2021-04-30 16:04:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,34 +4,12 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-require("dotenv").config();
 var multiparty = require('multiparty');
 const port = process.env.PORT || 8080;
 //_________________________________//
-const AWS = require('aws-sdk')
-const fs = require('fs')
-const ACCESS_KEY_ID = "AKIAXNLLRY7K57RVYJ7D"
-const SECRET_ACCESS_KEY = process.env.AWS_Secret
-const BUCKET_NAME = "alexunicovidapi"
 app.get('/*',function(req,res){
     res.send("Welcome To Our API Post Your Data Please");
 });
-//var s3 = new AWS.S3({
-//    accessKeyId: ACCESS_KEY_ID,
-//    secretAccessKey: SECRET_ACCESS_KEY,
-//})
-//var params = {
-//    Key: 'weights',
-//    Bucket: BUCKET_NAME
-//}
-//s3.getObject(params, function(err, data) {
-//    if (err) {
-//       throw err
-//    }
-//    fs.writeFileSync('./weights', data.Body)
-//    console.log('file downloaded successfully')
-//})
-
 app.post('/API',function(req,res){
     var start = new Date().getTime();
     var form = new multiparty.Form();
